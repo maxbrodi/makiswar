a85f03bf868d61edda6ef12c33ce2e2f80d43e98
Maxime Braud <mx.braud@gmail.com>
2015-06-29 16:37:48 +0200

    Add heroku deploy script

---
+++ Gemfile
@@ -0,0 +1,41 @@
+source "https://rubygems.org"
+
+ruby "2.2.2"
+
+gem "rails", "4.2.3"
+gem "pg"
+gem "figaro"
+gem "simple_form"
+gem "country_select"
+gem "high_voltage"
+gem "devise"
+# gem "pundit"
+gem "paperclip"
+gem "aws-sdk", "< 2.0"
+
+gem "jquery-rails"
+gem "sass-rails", "~> 5.0"
+gem "uglifier"
+gem "bootstrap-sass"
+gem "font-awesome-sass"
+
+gem "rails-i18n"
+gem "devise-i18n"
+gem "devise-i18n-views"
+
+group :development, :test do
+  gem "spring"
+  gem "annotate"
+  gem "binding_of_caller"
+  gem "better_errors"
+  gem "quiet_assets"
+  gem "pry-byebug"
+  gem "pry-rails"
+  gem "letter_opener"
+end
+
+group :production do
+  gem "rails_12factor"
+  gem "puma"
+  gem "rack-timeout"
+end
---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,27 @@
+class ApplicationController < ActionController::Base
+  include Pundit
+
+  protect_from_forgery with: :exception
+
+  before_action :authenticate_user!, unless: :pages_controller?
+
+  after_action :verify_authorized, except:  :index, unless: :devise_or_pages_controller?
+  after_action :verify_policy_scoped, only: :index, unless: :devise_or_pages_controller?
+
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
+  private
+
+  def devise_or_pages_controller?
+    devise_controller? || pages_controller?
+  end
+
+  def pages_controller?
+    controller_name == "pages"  # Brought by the `high_voltage` gem
+  end
+
+  def user_not_authorized
+    flash[:error] = I18n.t('controllers.application.user_not_authorized', default: "You can't access this page.")
+    redirect_to(root_path)
+  end
+end
