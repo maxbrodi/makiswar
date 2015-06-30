73bcaea45dd817f5990caf6aafe0ccf6b02f9090
Maxime Braud <mx.braud@gmail.com>
2015-06-30 18:30:26 +0200

    place other makis on map. jquery and controller. needs view

--- app/assets/javascripts/app/worldmap.js
+++ app/assets/javascripts/app/worldmap.js
@@ -1,7 +1,24 @@
 $( document ).ready(function() {
+  // ajustement de la hauteur des divs
   var cellwidth = $('.cell').width();
   $('.cell').css("height", cellwidth);
   $('.worldmap').css("height", cellwidth*5);
+
+  // affichage des autres makis
+
+  $('.cell-info').each(function( index ) {
+    var x = $( this ).text().split('-')[1];
+    var y = $( this ).text().split('-')[2];
+    console.log(x);
+    console.log(y);
+    console.log(x + y);
+    $('*[data-cell="'+ x + y + '"]').addClass("salmon");
+//     if ($( this ).is(':empty')){
+//     //do something
+// }
+  });
+
+
 
 
   // .cell onclick
--- app/controllers/worlds_controller.rb
+++ app/controllers/worlds_controller.rb
@@ -3,12 +3,15 @@ class WorldsController < ApplicationController
     @x_player = current_user.x
     @y_player = current_user.y
 
+    @x_shift = @x_player - 3
+    @y_shift = @y_player - 3
+
     @players = User.in_area(current_user)
 
     @players_on_map = {}
 
     @players.each do |player|
-      players_on_map[(player[:x] + player[:y]).to_sym] = player
+      @players_on_map[("#{player[:x] - @x_shift}#{player[:y] - @y_shift}").to_sym] = player
     end
 
     # for x_ennemy in (@x_player - 2)..(@x_player + 2)
