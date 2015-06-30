cd85ee90b08270bf7c628034d9e1415537b100c3
gregoire charles <gcm.charles@gmail.com>
2015-06-30 15:15:22 +0200

    user registration / session done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,35 @@
 class ApplicationController < ActionController::Base
   # include Pundit
-
+  before_action :set_locale
   protect_from_forgery with: :exception
 
   before_action :authenticate_user!, unless: :pages_controller?
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   # after_action :verify_authorized, except:  :index, unless: :devise_or_pages_controller?
   # after_action :verify_policy_scoped, only: :index, unless: :devise_or_pages_controller?
 
   # rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) << :name
+  end
+
+  def after_sign_in_path_for(resource)
+    worlds_show_path
+  end
+
   private
+
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
+  end
+
+  def default_url_options
+    { locale: I18n.locale == I18n.default_locale ? nil : I18n.locale }
+  end
 
   def devise_or_pages_controller?
     devise_controller? || pages_controller?
