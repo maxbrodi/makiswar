9374c1de228af2798151c4a7d58805d84e684b4a
Maxime Braud <mx.braud@gmail.com>
2015-07-01 19:13:59 +0200

    fight front ok without design

--- app/assets/javascripts/app/fight_display.js
+++ app/assets/javascripts/app/fight_display.js
@@ -1,5 +1,17 @@
  $( document ).ready(function() {
   // ajustement de la hauteur des divs
-  var fightwidth = $('.fight-display').width();
-  $('.fight-display').css("height", fightwidth);
+  var fightSize = $('.fight-display').width();
+  var infoHeight = $(window).innerHeight() - fightSize;
+
+  $('.fight-display').css("height", fightSize);
+  $('.weapon-selection').css("height", infoHeight);
+
+  infoHeight = $('.weapon-selection').height();
+  var itemSelectionHeight = infoHeight - 70;
+
+  $('.item-selection-box').css("height", itemSelectionHeight);
+  if (itemSelectionHeight < 150 ){
+    $('.item-attack-display').addClass('hidden')
+    $('h2').addClass('smalldevice')
+  };
 });
