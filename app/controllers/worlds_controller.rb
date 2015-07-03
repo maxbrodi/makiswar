6a5766cfba9707fa66ecfd0a6ee86dc724cd07d7
Maxime Braud <mx.braud@gmail.com>
2015-07-03 10:48:14 +0200

    refacto worlds controller

--- app/controllers/worlds_controller.rb
+++ app/controllers/worlds_controller.rb
@@ -2,19 +2,17 @@ class WorldsController < ApplicationController
   respond_to :js, only: [:update]
 
   def show
-    worldmap
+    set_player_position
+    position_other_players_in_grid
+    set_world_info
   end
 
   def update
-    worldmap
 
-    @new_position = params[:new_position].split("")
-    current_user.x = @new_position[0].to_i + @x_shift
-    current_user.y = @new_position[1].to_i + @y_shift
-    current_user.save
-
-    @x_shift = current_user.x - 3
-    @y_shift = current_user.y - 3
+    set_player_position
+    translate_player_position
+    position_other_players_in_grid
+    set_world_info
 
     respond_to do |format|
       format.js
@@ -23,22 +21,35 @@ class WorldsController < ApplicationController
 
   private
 
-  def worldmap
+  def set_player_position
     @x_player = current_user.x
     @y_player = current_user.y
 
     # Translation
     @x_shift = @x_player - 3
     @y_shift = @y_player - 3
+  end
 
+  def translate_player_position
+    @new_position = params[:new_position].split("")
+    current_user.x = @new_position[0].to_i + @x_shift
+    current_user.y = @new_position[1].to_i + @y_shift
+    current_user.save
+
+    @x_shift = current_user.x - 3
+    @y_shift = current_user.y - 3
+  end
+
+  def position_other_players_in_grid
     @players = User.in_area(current_user)
     @players_on_map = {}
 
-    # binding.pry
     @players.each do |player|
      @players_on_map[("#{player[:x] - @x_shift}#{player[:y] - @y_shift}").to_sym] = player
     end
+  end
 
+  def set_world_info
     # Infos top
     @life = current_user.life
     @soja = current_user.soja
