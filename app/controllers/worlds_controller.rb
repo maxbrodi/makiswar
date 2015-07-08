f060acb9abfc1222d3e5910450c672c34143a51a
gregoire charles <gcm.charles@gmail.com>
2015-07-08 10:55:28 +0200

    world show items done

--- app/controllers/worlds_controller.rb
+++ app/controllers/worlds_controller.rb
@@ -28,29 +28,28 @@ class WorldsController < ApplicationController
   def set_player_position
     @x_player = current_user.x
     @y_player = current_user.y
-
-    # Translation
     @x_shift = @x_player - 3
     @y_shift = @y_player - 3
   end
 
   def translate_player_position
+    @item_type_id = params[:item_type_id]
     @new_position = params[:new_position].split("")
     current_user.x = @new_position[0].to_i + @x_shift
     current_user.y = @new_position[1].to_i + @y_shift
-    @item_type_id = params[:item_type_id]
-
     if @item_type_id == 'feet'
       @consumption = 4
     else
       item = current_user.items.where(item_type_id: @item_type_id).first
-      # item. # decrement life
-      @consumption = item.consumption
+      @consumption = item.item_type.consumption
+      item.broken_count += 1
+      item.save
+      if item.broken_count >= item.item_type.lifetime
+        item.destroy
+      end
     end
-
     current_user.soja -= @consumption
     current_user.save
-
     @x_shift = current_user.x - 3
     @y_shift = current_user.y - 3
   end
@@ -62,7 +61,6 @@ class WorldsController < ApplicationController
   def position_other_players_in_grid
     @players = User.in_area(current_user)
     @players_on_map = {}
-
     @players.each do |player|
      @players_on_map[("#{player[:x] - @x_shift}#{player[:y] - @y_shift}").to_sym] = player
     end
@@ -71,11 +69,9 @@ class WorldsController < ApplicationController
   def position_items_in_grid
     @items = Item.in_area(current_user)
     @items_on_map = {}
-
     @items.each do |item|
       @items_on_map[("#{item[:x] - @x_shift}#{item[:y] - @y_shift}").to_sym] = item
     end
-
   end
 
   def kinder_surprise
@@ -94,7 +90,6 @@ class WorldsController < ApplicationController
 
   def check_if_move_or_grab_update
     @choice = params[:choice]
-
     case @choice
       when "move" then translate_player_position
       when "grab" then add_item_to_user
@@ -102,69 +97,47 @@ class WorldsController < ApplicationController
   end
 
   def set_world_info
-    # Infos top
     @life = current_user.life
     @soja = current_user.soja
-
-    # ennemies
     case current_user.crew
-    when "avocado"
-     @ennemies = "salmons"
-    when "salmon"
-     @ennemies = "avocados"
-    when "bastardo"
-     @ennemies = "everyone, you bastard"
-    when "babyrice"
-     @ennemies = "everyone"
+      when "avocado" then @ennemies = "salmons"
+      when "salmon" then @ennemies = "avocados"
+      when "bastardo" then @ennemies = "everyone, you bastard"
+      when "babyrice" then @ennemies = "everyone"
     end
-
-    # info
     @info = ".info" + (rand(5) + 1).to_s
   end
 
   def sojajauge
-
     @soja = current_user.soja
-
     case @soja
-    when 0
-      @jauge = "empty.png"
-    when 1...3
-      @jauge = "verylow.png"
-    when 3...12
-      @jauge = "low.png"
-    when 12
-      @jauge = "half.png"
-    when 13...24
-      @jauge = "almostfull.png"
-    when 24
-      @jauge = "full.png"
-    when 25..48
-      @jauge = "overfull.gif"
+      when 0 then @jauge = "empty.png"
+      when 1...3 then @jauge = "verylow.png"
+      when 3...12 then @jauge = "low.png"
+      when 12 then @jauge = "half.png"
+      when 13...24 then @jauge = "almostfull.png"
+      when 24 then @jauge = "full.png"
+      when 25..48 then @jauge = "overfull.gif"
     end
-
-
   end
 
   def lifebar(player)
-
     case player.life
-    when 10
-      return "full"
-    when 6..10
-      return "almostfull"
-    when 5
-      return "half"
-    when 2..5
-      return "low"
-    when 1
-      return "verylow"
+      when 10
+        return "full"
+      when 6..10
+        return "almostfull"
+      when 5
+        return "half"
+      when 2..5
+        return "low"
+      when 1
+        return "verylow"
     end
   end
 
   def check_notif
     return unless current_user
-
     if current_user.life < 1
       redirect_to recaps_show_path
     else
@@ -172,5 +145,4 @@ class WorldsController < ApplicationController
       redirect_to recaps_show_path unless lastevent[:read]
     end
   end
-
 end
