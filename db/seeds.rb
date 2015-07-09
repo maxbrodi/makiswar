421a2c5f332cadfae5dc86da1d1b9cef94dc89e6
Maxime Braud <mx.braud@gmail.com>
2015-07-09 17:36:58 +0200

    new items

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,45 +7,38 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 
-World.create(name: "Forest", description: "Un monde cool", background: "lorem", max_x: 20, max_y: 20)
+# World.create(name: "Forest", description: "Un monde cool", background: "lorem", max_x: 20, max_y: 20)
 
-ItemType.create(name: "Sword", joke: "En garde chevalier", picture: "sword.png", kind:"Attack", lifetime: 5, consumption: 3, life_impact: 3)
-ItemType.create(name: "Fireball", joke: "Une bonne grosse boule", picture: "fireball.png", kind:"Attack", lifetime: 5, consumption: 4, life_impact: 4)
-ItemType.create(name: "Skate", joke: "Ride comme Tony Hawk", picture: "skate.png", kind:"Movement", lifetime: 5, consumption: 2)
-ItemType.create(name: "UberPop", joke: "Cheap but fun!", picture: "car.png", kind:"Movement", lifetime: 10, consumption: 1)
+# WEAPONS
 
-ItemType.create(name: "Wasabi", joke: "Miam", picture: "lorem", kind:"Healing", lifetime: 1)
+ItemType.create(name: "Valyrian Sword", joke: "Careful, Your Grace. Nothing cuts like Valyrian steel.", picture: "sword.png", kind:"Attack", lifetime: 10, consumption: 3, life_impact: 4)
+ItemType.create(name: "Baseball Bat", joke: "Favourite weapon of the Batman.", picture: "baseballbat.png", kind:"Attack", lifetime: 10, consumption: 3, life_impact: 2)
+ItemType.create(name: "Cocktail Poliakov", joke: "Hurts your opponent right in the liver.", picture: "cocktailpoliakof.png", kind:"Attack", lifetime: 10, consumption: 2, life_impact: 1)
+ItemType.create(name: "Banana Snake", joke: "Half banana, half snake, half awesome.", picture: "bananasnake.png", kind:"Attack", lifetime: 10, consumption: 2, life_impact: 2)
+
+# TRANSPORT TYPES
+
+ItemType.create(name: "Skate", joke: "Ride like Tuna Hawk.", picture: "skate.png", kind:"Movement", lifetime: 10, consumption: 3)
+ItemType.create(name: "UberPop", joke: "Still not forbidden in here.", picture: "uberpop.png", kind:"Movement", lifetime: 10, consumption: 2)
+ItemType.create(name: "Rainbow Unicorn", joke: "You look fabulous on it.", picture: "unicorn.png", kind:"Movement", lifetime: 10, consumption: 2)
+ItemType.create(name: "Hyperloop", joke: "You\'ve never moved that fast to the next cell.", picture: "hyperloop.png", kind:"Movement", lifetime: 10, consumption: 1)
 
 
-Item.create(item_type_id: 5, world_id: 1, x: 10, y: 11)
-Item.create(item_type_id: 5, world_id: 1, x: 10, y: 11)
-Item.create(item_type_id: 5, world_id: 1, x: 10, y: 11)
-Item.create(item_type_id: 5, world_id: 1, x: 10, y: 11)
-Item.create(item_type_id: 5, world_id: 1, x: 10, y: 11)
-Item.create(item_type_id: 5, world_id: 1, x: 10, y: 11)
+# (1...6).each do |i|
+(14...19).each do |i|
+  20.times do
+    x_rand = rand(21)
+    y_rand = rand(21)
+    Item.create(item_type_id: i, world_id: 1, x: x_rand, y: y_rand)
+  end
+end
 
-Item.create(item_type_id: 4, world_id: 1, x: 10, y: 11)
-Item.create(item_type_id: 4, world_id: 1, x: 10, y: 11)
-Item.create(item_type_id: 4, world_id: 1, x: 10, y: 11)
-Item.create(item_type_id: 4, world_id: 1, x: 10, y: 11)
-Item.create(item_type_id: 4, world_id: 1, x: 10, y: 11)
-Item.create(item_type_id: 4, world_id: 1, x: 10, y: 11)
+# (7...8).each do |i|
+(20...21).each do |i|
+  5.times do
+    x_rand = rand(21)
+    y_rand = rand(21)
+    Item.create(item_type_id: i, world_id: 1, x: x_rand, y: y_rand)
+  end
+end
 
-Item.create(item_type_id: 3, world_id: 1, x: 10, y: 11)
-Item.create(item_type_id: 3, world_id: 1, x: 10, y: 11)
-Item.create(item_type_id: 3, world_id: 1, x: 10, y: 11)
-Item.create(item_type_id: 3, world_id: 1, x: 10, y: 11)
-Item.create(item_type_id: 3, world_id: 1, x: 10, y: 11)
-Item.create(item_type_id: 3, world_id: 1, x: 10, y: 11)
-
-Item.create(item_type_id: 2, world_id: 1, x: 10, y: 11)
-Item.create(item_type_id: 2, world_id: 1, x: 10, y: 11)
-Item.create(item_type_id: 2, world_id: 1, x: 10, y: 11)
-Item.create(item_type_id: 2, world_id: 1, x: 10, y: 11)
-Item.create(item_type_id: 2, world_id: 1, x: 10, y: 11)
-
-Item.create(item_type_id: 1, world_id: 1, x: 10, y: 11)
-Item.create(item_type_id: 1, world_id: 1, x: 10, y: 11)
-Item.create(item_type_id: 1, world_id: 1, x: 10, y: 11)
-Item.create(item_type_id: 1, world_id: 1, x: 10, y: 11)
-Item.create(item_type_id: 1, world_id: 1, x: 10, y: 11)
